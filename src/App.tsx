dee12766b6723b5861518520a5c4ad703ee75aba
Михаил Белов <mihailbelov@Mihails-MacBook-Pro.local>
2025-03-04 11:48:12 +0100

    fit: create themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import "./App.css";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 export default function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app"></div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
